e2f9c0c0e8b96e44c5410c242d0609ef36b5ee4e
Julius Seporaitis <julius@yplanapp.com>
2016-03-18 11:59:14 +0000

    Make Popen.communicate return output as strings not bytes.

--- tests/test_ghostscript.py
+++ tests/test_ghostscript.py
@@ -7,6 +7,7 @@ class GhostscriptTest(unittest.TestCase):
     def test_installed(self):
         process = subprocess.Popen(
             ['gs', '--version'],
+            universal_newlines=True,
             stdin=None,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
@@ -14,5 +15,5 @@ class GhostscriptTest(unittest.TestCase):
         stdout, stderr = process.communicate()
 
         self.assertEqual(process.returncode, 0)
-        self.assertEqual(str(stderr), "")
-        self.assertRegexpMatches(str(stdout), r'9\.\d\d')
+        self.assertEqual(stderr, "")
+        self.assertRegexpMatches(stdout, r'9\.\d\d')
