cda51359a92e69e91a0a20d8d53ddb03076f02b4
Julius Seporaitis <julius@yplanapp.com>
2016-03-22 17:47:29 +0000

    WAT?!

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ test_requirements = [
 setup(
     name='treepoem',
     version=version,
-    description="Barcode rendering wrapper around supporting QRcode, Aztec, PDF417, I25, Code128, Code39 and many more types.",
+    description="Barcode rendering for Python 2 and 3 supporting QRcode, Aztec, PDF417, I25, Code128, Code39 and many more types.",
     long_description=readme + '\n\n' + history,
     author="Christian Muirhead",
     author_email='xtian@babbageclunk.com',
