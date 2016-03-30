5a71ca1f6b1cfea4bb07af62fe8ab3451990e1cd
Adam Chainz <adam@adamj.eu>
2016-03-30 17:01:49 +0100

    Use nose-parameterized for parameterized tests

--- tests/test_treepoem.py
+++ tests/test_treepoem.py
@@ -1,38 +1,37 @@
 from os import path
 from PIL import Image, ImageChops
 
+from nose_parameterized import parameterized
+
 import treepoem
 
 
-BARCODE_TYPES = (
+@parameterized([
     ('qrcode', "This is qrcode barcode."),
     ('azteccode', "This is azteccode barcode."),
     ('pdf417', "This is pdf417 barcode."),
     ('interleaved2of5', "0123456789"),
     ('code128', "This is code128 barcode."),
     ('code39', "THIS IS CODE39 BARCODE."),
-)
+])
+def test_barcode(barcode_type, barcode_data):
+    actual = treepoem.generate_barcode(
+        barcode_type,
+        barcode_data,
+        {}
+    )
 
+    fixture_path = "{dirname}/fixtures/{barcode_type}.png".format(
+        dirname=path.dirname(__file__),
+        barcode_type=barcode_type,
+    )
 
-def test_barcode():
-    for barcode_type, barcode_data in BARCODE_TYPES:
-        actual = treepoem.generate_barcode(
-            barcode_type,
-            barcode_data,
-            {}
-        )
+    # Trying to prevent a `ResourceWarning`.
+    # Bug: https://github.com/python-pillow/Pillow/issues/1144
+    # Workaround: https://github.com/python-pillow/Pillow/issues/835
+    with open(fixture_path, 'rb') as fixture:
+        expected = Image.open(fixture)
+        bbox = ImageChops.difference(actual, expected).getbbox()
+        assert bbox is None
 
-        fixture_path = "{dirname}/fixtures/{barcode_type}.png".format(
-            dirname=path.dirname(__file__),
-            barcode_type=barcode_type,
-        )
-
-        # Trying to prevent a `ResourceWarning`.
-        # Bug: https://github.com/python-pillow/Pillow/issues/1144
-        # Workaround: https://github.com/python-pillow/Pillow/issues/835
-        with open(fixture_path, 'rb') as fixture:
-            expected = Image.open(fixture)
-            bbox = ImageChops.difference(actual, expected).getbbox()
-            assert bbox is None
-
-        actual.close()
+    actual.close()
