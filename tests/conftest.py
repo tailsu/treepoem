0a492d5694bc9353639a2c1f34125a865f40f087
Adam Chainz <adam@adamj.eu>
2016-03-30 16:59:19 +0100

    Convert tests to pytest-style, move ghostscript check to run before any other tests and report version in test output

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+from __future__ import print_function, unicode_literals
+
+import re
+import subprocess
+import sys
+
+
+GHOSTSCRIPT_VERSION = subprocess.check_output(['gs', '--version']).decode('utf-8')
+if not re.match(r'9\.\d\d', GHOSTSCRIPT_VERSION):
+    print(
+        "Ghostscript must be version 9.X, have {}".format(GHOSTSCRIPT_VERSION)
+    )
+    sys.exit(1)
+
+
+def pytest_report_header(config):
+    return "Ghostscript version: {}".format(GHOSTSCRIPT_VERSION)
--- tests/test_treepoem.py
+++ tests/test_treepoem.py
@@ -1,44 +1,38 @@
-import unittest
 from os import path
 from PIL import Image, ImageChops
 
 import treepoem
 
 
-class TreepoemTestCase(unittest.TestCase):
+BARCODE_TYPES = (
+    ('qrcode', "This is qrcode barcode."),
+    ('azteccode', "This is azteccode barcode."),
+    ('pdf417', "This is pdf417 barcode."),
+    ('interleaved2of5', "0123456789"),
+    ('code128', "This is code128 barcode."),
+    ('code39', "THIS IS CODE39 BARCODE."),
+)
 
-    BARCODE_TYPES = (
-        ('qrcode', "This is qrcode barcode."),
-        ('azteccode', "This is azteccode barcode."),
-        ('pdf417', "This is pdf417 barcode."),
-        ('interleaved2of5', "0123456789"),
-        ('code128', "This is code128 barcode."),
-        ('code39', "THIS IS CODE39 BARCODE."),
-    )
 
-    def test_barcode(self):
-        for barcode_type, barcode_data in self.BARCODE_TYPES:
-            actual = treepoem.generate_barcode(
-                barcode_type,
-                barcode_data,
-                {}
-            )
+def test_barcode():
+    for barcode_type, barcode_data in BARCODE_TYPES:
+        actual = treepoem.generate_barcode(
+            barcode_type,
+            barcode_data,
+            {}
+        )
 
-            fixture_path = "{dirname}/fixtures/{barcode_type}.png".format(
-                dirname=path.dirname(__file__),
-                barcode_type=barcode_type,
-            )
+        fixture_path = "{dirname}/fixtures/{barcode_type}.png".format(
+            dirname=path.dirname(__file__),
+            barcode_type=barcode_type,
+        )
 
-            # Trying to prevent a `ResourceWarning`.
-            # Bug: https://github.com/python-pillow/Pillow/issues/1144
-            # Workaround: https://github.com/python-pillow/Pillow/issues/835
-            with open(fixture_path, 'rb') as fixture:
-                expected = Image.open(fixture)
-                self.assertIsNone(
-                    ImageChops.difference(actual, expected).getbbox(),
-                    msg="{barcode_type} barcode did not match.".format(
-                        barcode_type=barcode_type
-                    ),
-                )
+        # Trying to prevent a `ResourceWarning`.
+        # Bug: https://github.com/python-pillow/Pillow/issues/1144
+        # Workaround: https://github.com/python-pillow/Pillow/issues/835
+        with open(fixture_path, 'rb') as fixture:
+            expected = Image.open(fixture)
+            bbox = ImageChops.difference(actual, expected).getbbox()
+            assert bbox is None
 
-            actual.close()
+        actual.close()
